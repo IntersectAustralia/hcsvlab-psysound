37c56af7cfb2bef9f5cb11e5b282d9689f5e0ea4
elysewise2076 <elyse.wise@gmail.com>
2013-12-01 04:35:43 -0800

    add buildFhs script

---
+++ buildFhs.m
@@ -0,0 +1,58 @@
+
+function buildFhs(varargin)
+%interpret the args as arrays: infiles, coeffs, outfile
+
+infiles = {}
+outfile = ''
+coeffs = {}
+currentArg = 0
+capture = 0
+
+for i = 1:length(varargin)
+
+  % find which argument we are currently inspecting
+  if strcmp(varargin{i}, '-i')
+    currentArg = 1
+    capture = 0
+  elseif strcmp(varargin{i}, '-c')
+    currentArg = 2
+    capture = 0
+  elseif strcmp(varargin{i}, '-o')
+    currentArg = 3
+    capture = 0
+  else
+    currentArg = currentArg
+    capture = 1
+  end
+
+  if (currentArg == 1 && capture == 1) %infile
+    infiles{length(infiles) + 1} = varargin{i}
+  elseif (currentArg == 2 && capture == 1) %coeffs
+    coeffs{length(coeffs) + 1} = str2double(varargin{i})
+  elseif (currentArg == 3 && capture == 1) %outfile    
+    outfile = varargin{i}
+  end  
+
+
+end
+  infiles
+  coeffs
+  outfile
+execBuildFhs(infiles, coeffs, outfile)
+end
+
+
+function execBuildFhs(infiles, coeffs, outfile)
+disp('in files:');
+disp(infiles);
+psysound3;
+
+fhs = readData(infiles);
+for i = 1:length(coeffs)
+  fhs(i).calCoeff = coeffs{i}
+end
+
+save ./temp/buildFhsOutput.mat fhs
+copyfile('./temp/buildFhsOutput.mat', outfile)
+
+end
